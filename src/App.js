62491bef501034afae2449e5b9ae848e5f64b9bf
AustinKo <skygg@naver.com>
2020-11-28 20:35:29 +0900

    헤더 구성 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Header from "./header";
+import styled from "styled-components"
 
 //AND 연산자(&&)를 사용한 조건부 렌더링
 function App() {
-    const name = '뤼액트';
+
+
     return (
-        <div>
-            {name ==='리액트' ? (<h1>리액트베이비.</h1>) : (<h2>리액트가 뭐라누</h2>)}
-        </div>
-    );
+    <Wrapper>
+        <Header/>
+    </Wrapper>
+
+)
+    ;
 }
 
+
 export default App;
+const Wrapper = styled.div`
+    width : 100%;
+    
+`
---
+++ src/header.js
@@ -0,0 +1,75 @@
+import React, {Fragment} from 'react'
+import styled from "styled-components";
+
+const HeaderWrap = styled.div`
+    height : 100px;
+    width : 100%;
+    background : black;
+    display : table;
+`;
+
+const LogoImg = styled.img`
+    width : 40px;
+    height  : 40px;
+`;
+const LogoText = styled.p`
+    font-size :25px;
+    color : aqua;
+    font-weight : bolder;
+`;
+
+const logoZone = {
+    verticalAlign: "middle",
+    width : "50%",
+    display : "inline-block"
+
+}
+const Link = styled.a`
+     color : orangeRed;
+     font-size : 20px;
+     text-decoration : none;
+     float : right;
+     line-height : 100px;
+     margin-right : 20px;
+`;
+const Links = styled.div`
+    display : inline-block;
+    
+`
+const thirdLinkStyle ={
+    marginRight : "200px"
+}
+
+function Header() {
+
+    const imagePath = "./logo192.png";
+    return (
+        <div>
+            <HeaderWrap>
+                <table style={logoZone}>
+                    <tr>
+                        <td>
+                            <LogoImg src={imagePath}/>
+                        </td>
+                        <td rowSpan={3}>
+                            <LogoText> Welcome React!</LogoText>
+                        </td>
+
+                    </tr>
+
+                </table>
+                <links>
+                <Link style={thirdLinkStyle}>로그인</Link>
+                <Link>로그인</Link>
+                <Link>로그인</Link>
+                </links>
+
+
+            </HeaderWrap>
+        </div>
+    );
+}
+
+
+export default Header;
+
